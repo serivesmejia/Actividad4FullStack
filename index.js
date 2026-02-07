a8c3f83d043f5447614276dbe9d2c7bf5af17d90
Sebastian Erives <serivesmejia@deltacv.org>
2026-02-07 01:20:13 -0600

    Usar routes en el index.js

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-
-const controller = require('./controllers/PedidosController');
 const express = require('express');
+const routes = require('./routes/pedidos.routes');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get('/productos', controller.getAll.bind(controller));
-app.get('/productos/:id', controller.getById.bind(controller));
-app.post('/productos/', controller.create.bind(controller));
-app.put('/productos/:id', controller.update.bind(controller));
-app.delete('/productos/:id', controller.delete.bind(controller));
+app.use('/pedidos', routes);
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
